d42b256186fa327329d21f27fd7644adedad87ff
trevorlink <lvpidadiao@hotmail.com>
2024-11-23 00:48:31 +0800

    initial start

---
+++ src/lib.rs
@@ -0,0 +1,20 @@
+pub mod audio_processor;
+pub mod config;
+pub mod server;
+pub mod utils;
+pub mod services;
+
+pub fn add(left: u64, right: u64) -> u64 {
+    left + right
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        let result = add(2, 2);
+        assert_eq!(result, 4);
+    }
+}
