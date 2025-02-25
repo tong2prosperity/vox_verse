2482ae72cecb2cd1fcbda920ce87feae7a74b2d0
trevorlink <trevorlink@hotmail.com>
2025-02-25 17:52:09 +0800

    at trying to add tts responder

--- vox_server/src/audio_processor/asr_processor.rs
+++ vox_server/src/audio_processor/asr_processor.rs
@@ -1,1 +1,13 @@
+use crate::server::data;
+use anyhow::Result;
+use llm_audio_toolkit::asr::{
+    tencent::{structs::*, *},
+    EchoMageConcurr,
+};
+use tokio::sync::mpsc;
 
+pub struct AsrProcessor {
+    asr_service: TencentASR,
+    data_tx: mpsc::Sender<structs::AudioData>,
+    result_rx: mpsc::Receiver<RecognitionResult>,
+}
--- vox_server/src/audio_processor/mod.rs
+++ vox_server/src/audio_processor/mod.rs
@@ -1,3 +1,4 @@
+pub mod asr_processor;
 pub mod biz_processor;
 
 use crate::{debug, error, info, warn};
